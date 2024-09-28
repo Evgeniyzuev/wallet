cea46f85ba77161832eae2ea65ffd30bbb1c26c4
Evgeniyzuev <evgeniyzuev43@gmail.com>
2024-09-28 16:34:28 +0300

    Revert "0.13"
    This reverts commit 3624fc46d0f5e32cb4150e167b3f7b5285618f7b.

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
-import WebApp from '@twa-dev/sdk'
+import { useUser } from '../hooks/useUser'
 
 // declare global {
 //   interface Window {
@@ -13,44 +13,18 @@ import WebApp from '@twa-dev/sdk'
 // }
 
 export default function Home() {
+  const { user, setUser, error: userError } = useUser()
   const [notification, setNotification] = useState('')
-  const [user, setUser] = useState<any | null>(null)
   const [error, setError] = useState<string | null>(null)
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const tg = WebApp
-      tg.ready()
-
-      const initDataUnsafe = tg.initDataUnsafe || {}
-
-      if (initDataUnsafe.user) {
-        fetch('/api/user', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(initDataUnsafe.user),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.error) {
-              setError(data.error)
-            } else {
-              setUser(data)
-            }
-          })
-          .catch((err) => {
-            console.log(err)
-            setError('Failed to fetch user data')
-          })
-      } else {
-        setError('No user data available')
-      }
-    } else {
-      setError('This app should be opened in Telegram')
-    }
-  }, [])
+  // interface UserData {
+  //   id: number;
+  //   first_name: string;
+  //   last_name?: string;
+  //   username?: string;
+  //   language_code: string;
+  //   is_premium?: boolean;
+  // }
 
 
 
