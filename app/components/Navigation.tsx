b1d348b5bdf7790603acd48c90ff34f1cb6a45bb
Evgeniyzuev <evgeniyzuev43@gmail.com>
2024-10-25 05:55:10 +0300

    0.4.20

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -5,7 +5,9 @@ import { usePathname } from 'next/navigation'
 import walletIcon from '../images/icon-wallet.svg';
 import tasksIcon from '../images/icon-tasks.svg';
 import friendsIcon from '../images/icon-friends.svg';
-import coinsIcon from '../images/ph_coins-fill.svg'; 
+import coinsIcon from '../images/ph_coins-fill.svg';
+import messageIcon from '../images/icon-message.png'; 
+import coreIcon from '../images/icon-core.png';
 
 import React from 'react';
 
@@ -13,8 +15,8 @@ export default function Navigation({ setCurrentPage }: { setCurrentPage: (page: 
   const pathname = usePathname()
 
   const navItems = [
-    { href: 'home', label: 'Core' },
-    { href: 'ai', label: 'Ai' },
+    { href: 'home', label: 'Core', icon: <img src={coreIcon.src} alt="Core" className="w-6 h-6" /> },
+    { href: 'ai', label: 'Ai', icon: <img src={messageIcon.src} alt="Ai" className="w-6 h-6" /> },
     { href: 'wallet', label: 'Wallet', icon: <img src={walletIcon.src} alt="Wallet" className="w-6 h-6" /> },
     { href: 'tasks', label: 'Tasks', icon: <img src={coinsIcon.src} alt="Tasks" className="w-6 h-6" /> },
     { href: 'friends', label: 'Frens', icon: <img src={friendsIcon.src} alt="Frens" className="w-6 h-6" /> },
@@ -29,7 +31,7 @@ export default function Navigation({ setCurrentPage }: { setCurrentPage: (page: 
           <button
             key={item.href}
             onClick={() => setCurrentPage(item.href)}
-            className={`font-medium flex flex-col items-center p-2 rounded-lg transition-all ${
+            className={`font-medium flex w-1/6 flex-col items-center p-2 rounded-lg transition-all ${
               pathname === item.href
                 ? 'text-blue-300 bg-gray-700' // добавляем фон для активной кнопки
                 : 'text-white hover:text-blue-300 hover:bg-gray-700'
